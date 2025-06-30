facb5a9181998ac85bd28c68cac51d8e9951a831
Mohamed Harish <mohamed.harish@cognizant.com>
2025-06-30 19:07:25 +0000

    header changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -111,6 +111,7 @@ export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  console(navPath);
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
