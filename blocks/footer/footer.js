c7d3a42437ff7c35b2367eba694449cda48f18ff
Mohamed Harish <mohamed.harish@cognizant.com>
2025-06-30 19:25:16 +0000

    footer changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -9,6 +9,7 @@ export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  console.log(footerPatrh);
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
